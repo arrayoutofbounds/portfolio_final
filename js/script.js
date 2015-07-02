4b78327948f70146406d6c40399f6afcf76daa59
Anmol Desai <anmoldesai@Anmols-MacBook-Pro.local>
2015-07-02 16:32:24 +1200

    arrows work

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 $(function(){
 
 // DOM is loaded
-
+	
+	// has parent li of the image that is clicked on
+	var current_li;
 	
 	$("#portfolio").on('click', 'img', function () {
     	// get path of image clicked
 		var source = $(this).attr("src");
+
+		// parent li of the source image
+		current_li = $(this).parent();
 
 		// frame has an empty image.....so set its source to the image clicked on!
 		$("#main").attr("src",source);
@@ -23,12 +28,36 @@ $(function(){
 
 	});
 
+	// when the right arrow is clicked
 	$("#right").click(function(){
 
+		if(current_li.is(":last-child")){
+			var next_li = $("#portfolio li").first();
+		}else{
+			var next_li = current_li.next();
+		}
+
+		var next_src = next_li.children("img").attr("src");
+		$("#main").attr("src",next_src);
+
+		current_li = next_li;
 	});
 
+
+	// when the left arrow is clicked
 	$("#left").click(function(){
 
+		if(current_li.is(":first-child")){
+			var prev_li = $("#portfolio li").last();
+		}else{
+			var prev_li = current_li.prev();
+		}
+
+		
+		var prev_src = prev_li.children("img").attr("src");
+		$("#main").attr("src",prev_src);
+
+		current_li = prev_li;
 	});
 
 
